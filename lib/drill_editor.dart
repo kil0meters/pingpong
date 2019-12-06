f5b0bdf337865041bb5fe974a73b5451fa26164a
kilometers <kilo@meters.sh>
2019-12-06 09:56:54 -0800

    Refactor drill editor

--- lib/drill_editor.dart
+++ lib/drill_editor.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'globals.dart' as globals;
+
 class Drill {
   const Drill(this.title, this.description, this.firingSpeed, this.oscillationFrequency, this.topspin, this.backspin);
   final String title;
@@ -8,6 +10,53 @@ class Drill {
   final int oscillationFrequency;
   final int topspin;
   final int backspin;
+}
+
+class DrillEditorSlider extends StatefulWidget {
+  final String title;
+  final double max;
+  final double min;
+
+  DrillEditorSlider({Key key, this.title, this.max, this.min}) : super(key: key);
+
+  @override
+  _DrillEditorSliderState createState() => _DrillEditorSliderState(rangeStart: min, rangeEnd: max);
+}
+
+class _DrillEditorSliderState extends State<DrillEditorSlider> {
+  double rangeStart;
+  double rangeEnd;
+
+  _DrillEditorSliderState({Key key, this.rangeStart, this.rangeEnd});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, 8, 0, 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            '${widget.title}: ${this.rangeStart.floor()}-${this.rangeEnd.floor()}',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          RangeSlider(
+            max: widget.max,
+            min: widget.min,
+            onChanged: (RangeValues values) {
+              setState(() {
+                rangeStart = values.start;
+                rangeEnd = values.end;
+              });
+            },
+            values: RangeValues(this.rangeStart, this.rangeEnd)
+          )
+        ],
+      ),
+    );
+  }
 }
 
 class DrillPage extends StatefulWidget {
@@ -29,7 +78,9 @@ class _DrillPageState extends State<DrillPage> {
               case '/':
                 return DrillList();
               case '/editor':
-                return DrillEditor();
+                return DrillEditorAutomatic();
+              default: 
+                return Text('ERROR - This should never happen');
             }
           }
         );
@@ -38,22 +89,74 @@ class _DrillPageState extends State<DrillPage> {
   }
 }
 
-class DrillEditor extends StatefulWidget {
-  DrillEditor({Key key}) : super(key: key);
+class DrillEditorAutomatic extends StatelessWidget {
+  DrillEditorAutomatic({Key key}) : super(key: key);
 
-  @override
-  _DrillEditorState createState() => _DrillEditorState();
-}
-
-class _DrillEditorState extends State<DrillEditor> {
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Editor'),
+        title: Text('Editor (Automatic)'),
         backgroundColor: Colors.green,
       ),
-      body: Text('wow')
+      body: Container(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            DrillEditorSlider(title: "Firing Speed", max: globals.firingSpeedMax, min: globals.firingSpeedMin),
+            DrillEditorSlider(title: "Oscillation Speed", max: globals.oscillationSpeedMax, min: globals.oscillationSpeedMin),
+            DrillEditorSlider(title: "Topspin", max: globals.topspinMax, min: globals.topspinMin),
+            DrillEditorSlider(title: "Backspin", max: globals.backspinMax, min: globals.topspinMin),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 8, 0, 8),
+              child: Column(
+
+                children: <Widget>[
+                  Text(
+                    'Selection Mode',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    )
+                  ),
+                  DrillEditorDropdown()
+                ]
+              )
+            )
+          ],
+        )
+      )
+    );
+  }
+}
+
+class DrillEditorDropdown extends StatefulWidget {
+  const DrillEditorDropdown({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _DrillEditorDropdownState createState() => _DrillEditorDropdownState();
+}
+
+class _DrillEditorDropdownState extends State<DrillEditorDropdown> {
+  String selectedValue = 'Random';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: selectedValue,
+      items: <String>['Random', 'Random Noise', 'Linear'].map((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+      onChanged: (String value) {
+        setState(() {
+          selectedValue = value;
+        });
+      }
     );
   }
 }
---
+++ lib/globals.dart
@@ -0,0 +1,13 @@
+library pingpong.globals;
+
+final double firingSpeedMax = 70;
+final double firingSpeedMin = 5;
+
+final double oscillationSpeedMax = 100;
+final double oscillationSpeedMin = 0;
+
+final double backspinMax = 100;
+final double backspinMin = 0;
+
+final double topspinMax = 100;
+final double topspinMin = 0;
